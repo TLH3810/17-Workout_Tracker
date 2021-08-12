b088bc6f9d7ee31beafb227827a58187de1a122c
Yorkie Fan <terahowell@gmail.com>
2021-08-11 22:33:26 -0400

    added in models and API routes

---
+++ Develop/routes/api.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create(req.body)
+    .then(dbWorkout => {res.json(dbWorkout);})
+    .catch(err => {res.status(400).json(err);});
+});
+
+
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find()
+  .then(dbWorkout => {res.json(dbWorkout);})
+  .catch(err => {res.status(400).json(err);});
+});
+
+router.put("/api/workouts/:id", ({body, params}, res) => {
+    db.Workout.findByIDAndUpdate(
+        params.id,
+        {$push: {exercises, body}},
+        {new:true, runValidators: true}
+    )
+    .then(dbWorkout => {res.json(dbWorkout);})
+    .catch(err => {res.status(400).json(err);});
+  });
+
+module.exports = router;
